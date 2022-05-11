b6c9921c3ca810664a88cd7aa021a880f8fad495
AnnaMintel <annikamintel99@yandex.ru>
2022-05-12 00:15:18 +0300

    refactor: added functionality

--- (2) authorization/script.js
+++ (2) authorization/script.js
@@ -4,25 +4,31 @@ const submit = () => {
     const phone = document.getElementById("phone");
     const email = document.getElementById("email");
 
-    if (((phone.value != "" && email.value == "") || (phone.value == "" && email.value != ""))
-        && name.value != "") {
+    if (name.value != "" && (phone.value != "" || email.value != "")) {
         name.classList.remove('error');
         phone.classList.remove('error');
         email.classList.remove('error');
         alert("Your data registered successfully!");
-    } else if (((phone.value == "" && email.value != "")) && name.value != "") {
-
-    
     } else if (phone.value == "" && email.value == "" && name.value == "") {
-    name.classList.add('error');
-    phone.classList.add('error');
-    email.classList.add('error');
-} else {
-    name.classList.remove('error');
-    phone.classList.remove('error');
-    email.classList.remove('error');
-    alert("Your data registered successfully!");
-}
-
+        name.classList.add('error');
+        phone.classList.add('error');
+        email.classList.add('error');
+    } else if (((phone.value != "" && email.value != "") && name.value == "")) {
+        name.classList.add('error');
+        phone.classList.remove('error');
+        email.classList.remove('error');
+    } else if ((phone.value == "" && email.value != "") && name.value == "") {
+        name.classList.add('error');
+        phone.classList.add('error');
+        email.classList.remove('error');
+    } else if ((phone.value != "" && email.value == "") && name.value == "") {
+        name.classList.add('error');
+        phone.classList.remove('error');
+        email.classList.add('error');
+    } else {
+        name.classList.remove('error');
+        phone.classList.add('error');
+        email.classList.add('error');
+    }
 
 }
